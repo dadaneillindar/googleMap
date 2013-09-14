c714a9b6f97b116ad3efde06822cad91b84887b1
DADA <dadaneillindar@gmail.com>
2013-09-14 10:51:12 +0800

    first commit

---
+++ src/tw/edu/tp/cksh/googlemap/MainActivity.java
@@ -0,0 +1,50 @@
+/*package tw.edu.tp.cksh.googlemap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}*/
+package tw.edu.tp.cksh.googlemap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity {
+    static final LatLng latLng = new LatLng(25.110480, 121.526229);
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker marker = map.addMarker(new MarkerOptions().position(latLng).title("家").snippet("中山北路6段290巷7弄5-1號5樓"));
+
+        // Move the camera instantly to latLng with a zoom of 16.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+    }
+}
