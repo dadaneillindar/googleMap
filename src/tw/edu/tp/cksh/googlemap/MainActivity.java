18d72d88ab170233ff38cea25c5320c045acc588
DADA <dadaneillindar@gmail.com>
2013-09-16 11:14:35 +0800

    add circle

--- src/tw/edu/tp/cksh/googlemap/MainActivity.java
+++ src/tw/edu/tp/cksh/googlemap/MainActivity.java
@@ -23,11 +23,14 @@ public class MainActivity extends Activity {
 package tw.edu.tp.cksh.googlemap;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -43,8 +46,9 @@ public class MainActivity extends Activity {
 
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         Marker marker = map.addMarker(new MarkerOptions().position(latLng).title("家").snippet("中山北路6段290巷7弄5-1號5樓"));
-
+        Marker marker_2 = map.addMarker(new MarkerOptions().position(new LatLng(25.115645, 121.519976)).title("阿嬤家").snippet("振華街2號2樓"));
+       Circle circle = map.addCircle(new CircleOptions().center(new LatLng(25.110700, 121.526229)).radius(100).strokeColor(Color.RED).fillColor(Color.CYAN));
         // Move the camera instantly to latLng with a zoom of 16.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(25.113000, 121.523000), 14));
     }
 }
